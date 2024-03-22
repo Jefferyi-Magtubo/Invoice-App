b008d314acd9b79935fa626721f5260005460799
jffryi <jefferyi.magtubo@gmail.com>
2024-03-22 18:53:52 -0500

    Started Project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Header from './components/Header'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Header />}>
+          <Route index element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Header() {
+
+    return(
+        <header>
+            <svg width="72" height="72"></svg>
+            <img src="src\assets\icon-check.svg" />
+        </header>  
+    )
+
+}
